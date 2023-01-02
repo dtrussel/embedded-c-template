4a0099c7c393c51a7528d7405fd27915d4384bb5
dtrussel <noreply@trussel.ch>
2023-01-02 17:27:26 +0100

    Enable source code based coverage reports

--- build.sh
+++ build.sh
@@ -7,13 +7,23 @@ BUILD_DIR=${SCRIPT_DIR}/build
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
+# native build
 mkdir -p clang-native
 cd clang-native
 cmake -G Ninja -D CMAKE_TOOLCHAIN_FILE=${SCRIPT_DIR}/toolchain/clang-native.cmake ../..
 ninja
-./mbed-tests
 
+# run unit tests
+export LLVM_PROFILE_FILE="mbed-tests.profraw"
+MERGED_PROFILE_DATA="mbed.profdata"
+IGNORE_FILES_OPTIONS=--ignore-filename-regex='.*_deps\/.*|.*test\/.*' # ignore all files from dependencies and unit tests files
+./mbed-tests
+llvm-profdata merge -sparse ${LLVM_PROFILE_FILE} -o ${MERGED_PROFILE_DATA}
+llvm-cov show ./mbed-tests --instr-profile=${MERGED_PROFILE_DATA} --format=html --output-dir=${BUILD_DIR}/coverage-report ${IGNORE_FILES_OPTIONS}
+llvm-cov report ./mbed-tests --instr-profile=${MERGED_PROFILE_DATA} ${IGNORE_FILES_OPTIONS}
 cd ..
+
+# target build
 mkdir -p gcc-arm-none-eabi
 cd gcc-arm-none-eabi
 cmake -G Ninja -D CMAKE_TOOLCHAIN_FILE=${SCRIPT_DIR}/toolchain/gcc-arm-none-eabi.cmake \
--- toolchain/clang-native.cmake
+++ toolchain/clang-native.cmake
@@ -1,3 +1,8 @@
-SET(CMAKE_ASM_COMPILER clang)
+set(CMAKE_ASM_COMPILER clang)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
+
+set(COVERAGE_GEN_FLAGS "-fprofile-instr-generate -fcoverage-mapping")
+
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${COVERAGE_GEN_FLAGS} ")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COVERAGE_GEN_FLAGS} ")
