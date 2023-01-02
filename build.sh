e770e011851bffef60f577d6b00322d1e2e3a3d9
dtrussel <noreply@trussel.ch>
2023-01-02 18:00:21 +0100

    Remove generating tarball for coverage report due to github API zip file limitations

--- build.sh
+++ build.sh
@@ -22,7 +22,6 @@ llvm-profdata merge -sparse ${LLVM_PROFILE_FILE} -o ${MERGED_PROFILE_DATA}
 llvm-cov show ./mbed-tests --instr-profile=${MERGED_PROFILE_DATA} --format=html --output-dir=${BUILD_DIR}/coverage-report ${IGNORE_FILES_OPTIONS}
 llvm-cov report ./mbed-tests --instr-profile=${MERGED_PROFILE_DATA} ${IGNORE_FILES_OPTIONS}
 cd ..
-tar czf coverage-report.tar.gz ${BUILD_DIR}/coverage-report
 
 # target build
 mkdir -p gcc-arm-none-eabi
