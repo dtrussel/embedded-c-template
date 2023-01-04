5016c40f143e6e1e36d610f4b28a2cef087e5441
dtrussel <noreply@trussel.ch>
2023-01-04 17:44:12 +0100

    Move specific compiler flags to toolchain files to avoid instrumenting gtest for coverage

--- toolchain/clang-native.cmake
+++ toolchain/clang-native.cmake
@@ -2,7 +2,7 @@ set(CMAKE_ASM_COMPILER clang)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-set(COVERAGE_GEN_FLAGS "-fprofile-instr-generate -fcoverage-mapping")
+set(MBED_COVERAGE_FLAGS -fprofile-instr-generate -fcoverage-mapping)
+set(MBED_WARNINGS_FLAGS -Wall -Wextra -pedantic -Werror)
 
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${COVERAGE_GEN_FLAGS} ")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COVERAGE_GEN_FLAGS} ")
+set(MBED_FLAGS ${MBED_COVERAGE_FLAGS} ${MBED_WARNINGS_FLAGS})
